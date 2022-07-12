bacd60117e95b01d2476d6ccb136c6e842f4fd26
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-07-12 13:57:02 +0300

    add Login component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.scss';
+import { useSelector } from 'react-redux/es/exports';
+
+export default function Button(props) {
+    const preferences = useSelector(state => state.preferences);
+    const color = preferences.color;
+    const theme = preferences.theme + '-theme';
+    return (
+        <button className={`${color} ${theme} button`}>{props.children}</button>
+    );
+}
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '../Container/Container';
+import './Login.scss';
+import { Link } from 'react-router-dom';
+import Icon from '../Icon/Icon';
+import { useSelector } from 'react-redux/es/exports';
+import Button from '../Button/Button';
+
 export default function Login() {
+    const preferences = useSelector(state => state.preferences);
+    const theme = preferences.theme + '-theme';
+    const [error, updateError] = useState(false);
     return (
         <Container>
             <h1>Влез в профил</h1>
-            <form action="">
+            <form id="login" className={theme}>
                 <div className="group">
-                    <label htmlFor="username">Потребтелско име</label>
+                    <label htmlFor="username">Потребителско име</label>
                     <input type="text" id="username" name="username" placeholder="Потребителско име" />
                 </div>
-                {/* <input type="text" name="email" placeholder="Имейл" /> */}
                 <div className="group">
                     <label htmlFor="password">Парола</label>
-                    <input type="text" id="password" name="password" placeholder="Парола" />
+                    <input type="password" id="password" name="password" placeholder="Парола" />
                 </div>
+                <div className="group">
+                    <p>Нямаш профил? <Link to="/register">Регистрирай се!</Link></p>
+                </div>
+                <Button><Icon icon="sign-in-alt"></Icon> Влез в профил</Button>
+                <p className={"error" + (!error ? " hidden" : null)}>Грешно потребителско име или парола!</p>
             </form>
         </Container>
     )
