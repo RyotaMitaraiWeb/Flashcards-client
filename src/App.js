62366b11f90101a45ed56069efc8df8c8e60f55d
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-07-17 18:42:44 +0300

    clean up + add React.Suspense

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
 import Login from './components/Login/Login';
-import { useSelector } from 'react-redux/es/exports';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { useEffect } from 'react';
+import requestService from './services/requests';
+import { updateUser } from './app/slices/user';
+import Logout from './components/Logout/Logout';
 
-// import LogoutBtn from './components/Logout';
-// import TestComponent from './components/Test';
+const Header = React.lazy(() => import('./components/Header/Header'));
+const Footer = React.lazy(() => import('./components/Footer/Footer'));
 
 function App() {
     const toggled = useSelector(state => state.modal.isToggled);
     const preferences = useSelector(state => state.preferences);
+    const user = useSelector(state => state.user);
     const theme = preferences.theme + '-theme';
+    const dispatch = useDispatch();
+    document.title = 'Начало';
+
+    useEffect(() => {
+        async function fetchData() {
+            const { res, data } = await requestService.get('/isLogged');
+            if (res.ok) {
+                dispatch(updateUser(data));
+            }
+        }
+
+        fetchData();
+    }, [dispatch]);
     return (
-        <main className={(toggled ? "locked " : "") + "app " + theme}>
-            <Header />
-            <Routes>
-                <Route path="/" element={<h2>Home</h2>}></Route>
-                <Route path="/about" element={<h2>За нас</h2>}></Route>
-                <Route path="/rules" element={<h2>Правила</h2>}></Route>
-                <Route path="/faq" element={<h2>Често задавани въпроси</h2>}></Route>
-                <Route path="/login" element={<Login />}></Route>
-            </Routes>
-            <Footer />
-        </main>
+        <React.Suspense fallback={<h1>Loading</h1>}>
+            <main className={(toggled ? "locked " : "") + "app " + theme}>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<h2>Home</h2>}></Route>
+                    <Route path="/about" element={<h2>За нас</h2>}></Route>
+                    <Route path="/rules" element={<h2>Правила</h2>}></Route>
+                    <Route path="/faq" element={<h2>Често задавани въпроси</h2>}></Route>
+                    <Route path="/login" element={<Login />}></Route>
+                    <Route path="/logout" element={<Logout />}></Route>
+                </Routes>
+                <Footer />
+
+            </main>
+        </React.Suspense>
     );
 }
 
