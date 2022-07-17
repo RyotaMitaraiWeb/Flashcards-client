4db513c56665cbab7b9d32cc80006951c26b510f
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-07-17 18:43:49 +0300

    add Logout functionality

---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import authService from '../../services/auth';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../app/slices/user';
+
+export default function Logout() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    useEffect(() => {
+        async function clearSession() {
+            const res = await authService.logout();
+            console.log(res.status + ' test');
+            dispatch(logout());
+            if (res.status === 204) {
+                navigate('/', { replace: true });
+            } else {
+                navigate('/login', { replace: true });
+            }
+        }
+
+        clearSession();
+    }, [dispatch, navigate]);
+}
