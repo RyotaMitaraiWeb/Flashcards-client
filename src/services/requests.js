ca26cc011f521f8bcb5f9222e44af9025290f7d8
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-07-17 18:41:30 +0300

    Add requests service

---
+++ src/services/requests.js
@@ -0,0 +1,42 @@
+async function request(method, endpoint, body) {
+    const url = 'http://localhost:5500' + endpoint;
+    const headers = {
+        'Access-Control-Allow-Origin': true,
+        'Access-Control-Allow-Credentials': true,
+    };
+    
+    const request = {
+        method,
+        credentials: 'include',
+        headers,
+    };
+
+    if (body) {
+        headers['Content-Type'] = 'application/json';
+        request.body = JSON.stringify(body);
+    }
+
+    const res = await fetch(url, request);
+
+    let data;
+
+    if (res.status !== 204) {
+        data = await res.json();
+    }
+
+    return { res, data };
+}
+
+const get = request.bind(null, 'GET');
+const post = request.bind(null, 'POST');
+const put = request.bind(null, 'PUT');
+const del = request.bind(null, 'DELETE');
+
+const requestService = {
+    get,
+    post,
+    put,
+    del
+};
+
+export default requestService;
