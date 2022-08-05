4ab4f9e76596a976392113f83717b847a4bbef73
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-08-05 12:11:18 +0300

    update DeckPreview with date formatter and turn buttons into links

--- src/components/Deck/DeckPreview.js
+++ src/components/Deck/DeckPreview.js
@@ -2,8 +2,12 @@ import Flashcard from "../Flashcard/Flashcard";
 import Icon from "../Icon/Icon";
 import './DeckPreview.scss';
 import { useSelector } from "react-redux/es/exports";
+import { Link } from "react-router-dom";
+import { useDateFormatter } from "../../hooks/useDateFormatter";
+
 export default function DeckPreview(props) {
     const user = useSelector(state => state.user.id);
+    const date = useDateFormatter(props.deck.createdAt);
 
     return (
         <div className="deck-preview">
@@ -11,15 +15,15 @@ export default function DeckPreview(props) {
                 <h3>{props.deck.title}</h3>
                 <div className="info">
                     <div className="creation-info">
-                        <div className="author">От <b>{props.deck.author}</b></div>
-                        <div className="date">{props.deck.date}</div>
+                        <div className="author">От <b>{props.deck.authorUsername}</b></div>
+                        <div className="date">{date}</div>
                     </div>
                     <div className="actions">
                         {user === props.deck.authorId
                             ? <button className="remove button"><Icon icon="times" /> Премахни</button>
                             : <>
-                                <button className="purple button"><Icon icon="pencil-alt" /> Редактирай</button>
-                                <button className="remove button"><Icon icon="trash-alt" /> Изтрии</button>
+                                <Link to={`/flashcard/${props.deck._id}/edit`} className="purple button"><Icon icon="pencil-alt" /> Редактирай</Link>
+                                <Link to={`/flashcard/${props.deck._id}/delete`} className="remove button"><Icon icon="trash-alt" /> Изтрии</Link>
                             </>
                         }
                     </div>
