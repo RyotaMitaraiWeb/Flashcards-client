2409adf0f0f6bd78da255b88a9f0148295aea6b6
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-08-05 12:12:53 +0300

    remove comments from Login.js

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -66,22 +66,3 @@ export default function Login() {
         </Container>
     )
 }
-
-// async function login() {
-//     const login = await fetch('http://localhost:5500/login', {
-//         method: 'POST',
-//         mode: 'cors',
-//         credentials: 'include',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Allow-Control-Access-Policy': true,
-//             "Access-Control-Allow-Credentials": true,
-//         },
-//         body: JSON.stringify({
-//             username: 'ryota',
-//             password: '123456',
-//         }),
-//     });
-//     const res = await login.json();
-//     console.log(res);
-// }
