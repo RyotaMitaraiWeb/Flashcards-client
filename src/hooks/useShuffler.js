3c1647fbda7862e0c344beccd738dcaed30337de
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-08-10 22:19:20 +0300

    make useShuffler create a new array, rather than modify the one passed to it

--- src/hooks/useShuffler.js
+++ src/hooks/useShuffler.js
@@ -1,11 +1,12 @@
 export function useShuffler(arr, run) {
+    const randomizedArray = [...arr];
     if (run) {
         const length = arr.length;
         for (let i = length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [arr[i], arr[j]] = [arr[j], arr[i]];
+            [randomizedArray[i], randomizedArray[j]] = [randomizedArray[j], randomizedArray[i]];
         }
     }
 
-    return arr;
+    return randomizedArray;
 }
