83e32c1e8bea323ecef80b413a8d5588eb23d0de
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-08-10 21:52:10 +0300

    remove console.log

--- src/hooks/useShuffler.js
+++ src/hooks/useShuffler.js
@@ -1,6 +1,5 @@
 export function useShuffler(arr, run) {
     if (run) {
-        console.log('random');
         const length = arr.length;
         for (let i = length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
